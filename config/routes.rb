6013dbbe5b68d4886ed6eff3b0c86128db1e8453
kyo02225 <quon.kyo.suica02225@gmail.com>
2019-12-23 12:39:34 +0900

    ログインサーバーサイド終了

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "todos#index"
   resources :todos
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update] 
 end
